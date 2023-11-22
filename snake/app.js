3583f79265e549d338e267f33ac355afd482686d
Victor Raul <v1c70rotakmer@gmail.com>
2023-11-22 16:24:58 -0500

    Add logic collision

--- snake/app.js
+++ snake/app.js
@@ -1,8 +1,11 @@
 const boardGame = document.querySelector('.board');
-const velocityRender = 2;
+const btnPlay = document.querySelector('#btn-play');
+const modalEndGame = document.querySelector('#end-game');
+
+const velocityRender = 6;
 const SNAKE = {
     size: 20,
-    speed: 2,
+    speed: 5,
     direction: 'static',
     body: [
         { x: 10, y: 5 },
@@ -11,7 +14,7 @@ const SNAKE = {
     ],
 };
 let lastRenderTime = 0;
-
+let FOOD = {x: 4, y: 5};
 
 // LOGICA DE LA SERPIENTE
 
@@ -30,26 +33,28 @@ const updateSnake = (snake) => {
     for (let i = snake.body.length - 2; i >= 0; i--) {
         snake.body[i + 1] = { ...snake.body[i] };
     }
+
     if (snake.direction === 'static') return;
-    if (snake.direction === 'up') snake.body[0].y -= 1;
-    if (snake.direction === 'down') snake.body[0].y += 1;
-    if (snake.direction === 'left') snake.body[0].x -= 1;
-    if (snake.direction === 'right') snake.body[0].x += 1;
+    else if (snake.direction === 'up') snake.body[0].y -= 1;
+    else if (snake.direction === 'down') snake.body[0].y += 1;
+    else if (snake.direction === 'left') snake.body[0].x -= 1;
+    else if (snake.direction === 'right') snake.body[0].x += 1;
+    console.log("move")
     handleLimits();
+    handleCollision();
+    eatFood();
 }
 const handleKeyDown = (event) => {
     const keyPress = event.key;
-    console.log(keyPress);
     if (keyPress === 'ArrowUp' && SNAKE.direction !== 'down')SNAKE.direction = 'up';
-    if (keyPress === 'ArrowDown' && SNAKE.direction !== 'up')SNAKE.direction = 'down';
-    if (keyPress === 'ArrowLeft' && SNAKE.direction !== 'right')SNAKE.direction = 'left';
-    if (keyPress === 'ArrowRight' && SNAKE.direction !== 'left') SNAKE.direction = 'right';
-
+    else if (keyPress === 'ArrowDown' && SNAKE.direction !== 'up')SNAKE.direction = 'down';
+    else if (keyPress === 'ArrowLeft' && SNAKE.direction !== 'right')SNAKE.direction = 'left';
+    else if (keyPress === 'ArrowRight' && SNAKE.direction !== 'left') SNAKE.direction = 'right';
+    console.log(SNAKE.direction);
     // quit game
     if (keyPress === 'Escape'){
         SNAKE.direction = 'static';
     }
-    
 }
 
 const handleLimits = () => {
@@ -58,23 +63,63 @@ const handleLimits = () => {
     if (SNAKE.body[0].y < 1) SNAKE.body[0].y = 20;
     if (SNAKE.body[0].y > 20) SNAKE.body[0].y = 1;
 }
-
+const eatFood = () => {
+    if (SNAKE.body[0].x === FOOD.x && SNAKE.body[0].y === FOOD.y) {
+        // Agregar un nuevo elemento al cuerpo de la serpiente en la ultima posicion
+        console.log(SNAKE.body);
+        SNAKE.body.push({ ...SNAKE.body[SNAKE.body.length - 1] });
+        console.log({ ...SNAKE.body[SNAKE.body.length - 1]});
+        console.log(SNAKE.body);
+        FOOD = newFood();
+    }
+}
+const handleCollision = () => {
+    // Si la cabeza de la serpiente colisiona con el cuerpo, se termina el juego
+    for (let i = 1; i < SNAKE.body.length; i++) {
+        if (SNAKE.body[0].x === SNAKE.body[i].x && SNAKE.body[0].y === SNAKE.body[i].y) {
+            endGame();
+        }
+    }
+}
 const handleEvents = () => {
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyDown);
 }
 
 
 
-
+//  LOGICA DE CONTROL DEL JUEGO
 const startGame = () => {}
 const resetGame = () => {}
-const drawFood = (ctx, food) => {}
+const endGame = () => {
+    SNAKE.direction = 'static';
+    SNAKE.body = [
+        { x: 10, y: 5 },
+        { x: 11, y: 5 },
+        { x: 12, y: 5 },
+    ];
+    FOOD = {x: 4, y: 5};
+    modalEndGame.classList.add('active');
+}
+
+const newFood = () => {
+    const foodX = Math.floor(Math.random() * 20) + 1;
+    const foodY = Math.floor(Math.random() * 20) + 1;
+    return { x: foodX, y: foodY };
+}
+const drawFood = () => {
+    const food = document.createElement('div');
+    food.style.gridColumnStart = FOOD.x;
+    food.style.gridRowStart = FOOD.y;
+    food.classList.add('food');
+    boardGame.appendChild(food);
+}
 const drawGame = () => {
     boardGame.innerHTML = '';
     drawSnake(SNAKE);
+    drawFood();
 }
+handleEvents(); 
 const update = () => {
-    handleEvents();
     updateSnake(SNAKE);
     drawGame();
 }
@@ -97,6 +142,6 @@ const mainLoop = (time) => {
     // Actualiza el tiempo del ultimo render
     lastRenderTime = time;
     update();
-    console.log(time);
+    // console.log(time);
 }
 window.requestAnimationFrame(mainLoop);
